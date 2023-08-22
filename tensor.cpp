f3ed9b36f4dce56e7652a2d6fe506ea9e69e5896
elbicuderri <muger1031@gmail.com>
2023-08-22 08:05:29 +0000

    test commit

--- tensor.cpp
+++ tensor.cpp
@@ -28,7 +28,8 @@ Tensor::Tensor(float *data, const vector<int> &shape_) {
   reshape(shape_);
   // buf = (float *)malloc(n * sizeof(float));
   CHECK_CUDA(cudaMallocHost((void **)&buf, n * sizeof(float)));
-  CHECK_CUDA(cudaMemcpy(buf, data, n * sizeof(float), cudaMemcpyHostToHost));
+  // CHECK_CUDA(cudaMemcpy(buf, data, n * sizeof(float), cudaMemcpyHostToHost));
+  CHECK_CUDA(cudaMemcpyAsync(buf, data, n * sizeof(float), cudaMemcpyHostToHost));
   // memcpy(buf, data, get_elem() * sizeof(float));
 }
 
